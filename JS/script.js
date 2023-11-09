b6fae309cc6d7898e01a17dd9ca790162494e403
Raviki <109937836+Raviki214@users.noreply.github.com>
2023-11-09 11:33:31 +0700

    pertama

---
+++ JS/script.js
@@ -0,0 +1,43 @@
+// let bb = document.getElementById("berat_badan");
+// let tb = document.getElementById("tinggi_badan");
+
+// function showResult() {
+//     if (bb.value === "" || tb.value === "") {
+//       result.innerHTML = "Diisi dulu";
+//     } else {
+//       const beratBadan = parseFloat(bb.value);
+//       const tinggiBadan = parseFloat(tb.value);
+//       const tb2 = parseFloat(tinggiBadan) / 100;
+
+//       const bmi = beratBadan / (tb2 * tb2);
+//     if (bmi < 18.5) {
+//         result.innerHTML = "Kekurangan berat badan " + bmi.toFixed(3);
+//     } else if (bmi > 18.5 && bmi < 24.9) {
+//         result.innerHTML = "Normal (ideal) " + bmi.toFixed(3);
+//     } else if (bmi > 25.0 && bmi < 29.9) {
+//         result.innerHTML = "Kelebihan berat badan " + bmi.toFixed(3);
+//     } else if (bmi > 30.0) {
+//         result.innerHTML = "Kegemukan (Obesitas) " + bmi.toFixed(3);
+//      }
+//     }
+// }
+
+const beratBadan = document.getElementById("bb")
+const tinggiBadan = document.getElementById("tb")
+const buttonHitung = document.getElementById("hitung")
+
+beratBadan.addEventListener('input',function() {
+    const inputBb = beratBadan.ariaValueMax.trim()
+    if (validationBb(inputBb)){
+        console.log("FALSE")
+        buttonHitung.ariaDisabled = false;
+    }else{
+        console.log("TRUE")
+        buttonHitung.disabled = true;
+    }
+})
+
+function validationBb(input) {
+    const integerRegex = /^-?\d+5/;
+    return integerRegex.test(input)
+}
